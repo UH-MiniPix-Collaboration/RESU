219a7058360a9a1e9a8fcfcd199567babd00958f
rbmasek <r.masek97@gmail.com>
2018-04-03 14:22:50 -0500

    A byte is sent to the Arduino upon the Pi receiving a complete packet. Added logging processes.

--- interface/hasp_interface.py
+++ interface/hasp_interface.py
@@ -2,14 +2,56 @@
 
 #!/usr/bin/python3
 import serial
-from processcmd import processcmd
-ser = serial.Serial('/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller_D-if00-port0', 4800)
-print("Serial Connected")
+import io
+import datetime
+import time
 
-# read from Arduino
+log_file = io.open("log.txt", "a")
+ser = serial.Serial('/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller_D-if00-port0', 4800)
+
+
+log_file.write("---Begin Log Entry---\n")
+
+# Open serial port
+if(ser.isOpen() == False):
+        ser.open()
+        print("Serial Connected")
+        log_file.write("Serial Connected\n")
+else:
+        print("Serial Connected")
+        log_file.write("Serial Connected\n")
+
+        
+# Read from Arduino
 while True:
-    cmd1, cmd2 = processcmd(ser)
-    print(cmd1, cmd2)
+        s_input = ser.read(1) #.decode("utf8", "replace")
+        
+        if s_input == b'\x01':
+                ts = str(datetime.datetime.utcnow())    #Gets the time at execution in UTC   
+                log_file.write("Begin Arduino Transmission: " + str(ts) + "\n")
+        print(s_input)
+        log_file.write(str(s_input)+"\n")
+#        import pdb; pdb.set_trace()  //Debugger
+
+        if s_input == b'\x30':
+                ts = str(datetime.datetime.utcnow())    #Gets the time at execution in UTC   
+                log_file.write("End Arduino Transmission: " + str(ts) + "\n")
+                succ = ser.write(b'n')
+                ser.write(b'u')
+                ser.write(b'd')
+                ser.write(b'e')
+                ser.write(b's')
+
+                if succ == 1:
+                        print ("Success: " + str(succ) + "\n")
+                        log_file.write("Successful Transmission\n")
+
+                log_file.write("---End Log Entry---\n\n\n")
+        #input = ser.read(1) * 256
+	#input = input + ser.read()
+        #print ("Read input " + input.decode("utf-8") + " from Arduino")
+
+# write something back
 
 #ser.write(b'A')
 
